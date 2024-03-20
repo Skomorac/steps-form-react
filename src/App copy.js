2b75881000e7c9a96b42d2fb99a94993d38fd679
Skomorac <admir.skomorac@outlook.com>
2024-03-20 14:39:59 +0100

    children prop use case

--- src/App copy.js
+++ src/App copy.js
@@ -10,6 +10,7 @@ function App() {
   return (
     <div>
       <Steps />
+      <Steps />
     </div>
   );
 }
@@ -47,31 +48,22 @@ function Steps() {
           </p>
 
           <div className="buttons">
-            <Button
-              textColor={"#fff"}
-              bgColor={"#7950f2"}
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
               onClick={handlePrevious}
             >
-              <span>👈</span> Previous
-            </Button>
-            <Button textColor={"#fff"} bgColor={"#7950f2"} onClick={handleNext}>
-              Next <span>👉</span>
-            </Button>
+              Previous
+            </button>
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              onClick={handleNext}
+            >
+              Next
+            </button>
           </div>
         </div>
       )}
     </>
-  );
-}
-
-function Button({ textColor, bgColor, onClick, children }) {
-  return (
-    <button
-      style={{ backgroundColor: bgColor, color: textColor }}
-      onClick={onClick}
-    >
-      {children}
-    </button>
   );
 }
 
